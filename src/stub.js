e5971588a1d949d2726f5a47c59098b395945c56
cg1101 <cg1101@gmail.com>
2019-12-03 15:26:15 +1100

    feat: add greet

--- src/stub.js
+++ src/stub.js
@@ -7,3 +7,7 @@ export const func = (message) => {
 export const sleep = (ms) => {
   return new Promise((resolve, reject) => setTimeout(resolve, ms));
 }
+
+export const greet = (person) => {
+  console.log(`hello, ${person}`);
+}
